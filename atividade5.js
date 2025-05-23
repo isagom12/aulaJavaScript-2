11ef99ee0de7d70bae5db8f09381c78e6429e2fb
isabelle gomes <isabelle.santos.gomes@escola.pr.gov.br>
2025-05-23 09:07:19 -0300

    terminando as atividades

---
+++ atividade5.js
@@ -0,0 +1,9 @@
+function alternarVisibilidade(){
+    let caixa = document.getElementById("caixa");
+    if(caixa.style.display==="nome"){
+       caixa.style.display="block";
+
+    }else{
+        caixa.style.display = "nome";
+    }
+}
